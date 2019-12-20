021f59b6e6ea3e2ecedc9d3de824b96699bd3d17
fireinjun <mk@fireinjun.com>
2019-12-20 03:36:22 -0500

    added one automation script

---
+++ scripts/new_challenge.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+
+PATH=$PATH:~
+
+set_vars(){
+    DIR=$1
+    TEST="test_${1}.py"
+    CODE="$1.py"
+    INIT="__init__.py"
+}
+
+build_files()
+{
+    mkdir  $1 #Create Project Folder
+    pushd "$1" #Go into Project Folder
+    touch "$TEST"
+    touch "$CODE"
+    touch "$INIT" #Create new files
+    popd #Go back to last stored spot
+}
+
+main()
+{
+set_vars
+build_files
+}
+
+main
