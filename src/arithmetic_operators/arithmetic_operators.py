b06db5c10369c566f170bd8d0cf109add1ed7426
fireinjun <mk@fireinjun.com>
2019-12-24 13:34:29 -0500

    improved documentation

--- src/arithmetic_operators/arithmetic_operators.py
+++ src/arithmetic_operators/arithmetic_operators.py
@@ -1,21 +1,25 @@
-if __name__ == '__main__':
+if __name__ == "__main__":
     a = int(input())
     b = int(input())
 
 
-def line_one(a, b):  # The first line contains the sum of the two numbers.
+def line_one(a, b):
+    """ Return the sum of two *numbers*."""
     return a + b
 
 
-def line_two(a, b):  # The second line contains the difference of the two numbers(first - second).
+def line_two(a, b):
+    """ Return difference of  two *numbers*."""
     return a - b
 
 
-def line_three(a, b):  # The third line contains the product of the two numbers.
+def line_three(a, b):
+    """Return the product of the two *numbers*."""
     return a * b
 
 
-line_tuple = line_one(a, b), line_two(a, b), line_three(a, b)
-
-for n in line_tuple:
-    print(n)
+def solution(a, b):
+    """Print each value in the tuple 'line_tuple' on a new line."""
+    line_tuple = line_one(a, b), line_two(a, b), line_three(a, b)
+    for n in line_tuple:
+        print(n)
