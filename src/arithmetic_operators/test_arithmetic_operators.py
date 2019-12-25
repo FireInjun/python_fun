90a1d3317c1d384a2e440bc42703043cc1c9676a
fireinjun <mk@fireinjun.com>
2019-12-24 20:36:18 -0500

    fixed test file formatting

--- src/arithmetic_operators/test_arithmetic_operators.py
+++ src/arithmetic_operators/test_arithmetic_operators.py
@@ -5,26 +5,18 @@ from arithmetic_operators import prod_int
 
 
 class ArithmeticTestCase(unittest.TestCase):
-    """
-    Tests for `arithmetic_operator.py`
-    """
+    """ Tests for `arithmetic_operator.py` """
 
     def test_sum(self):
-        """
-        Is 2 and 5 combined equal to 7?
-        """
+        """ Is 2 and 5 combined equal to 7? """
         self.assertEqual(sum_int(2, 5), 7)
 
     def test_diff(self):
-        """
-        Does 7 takeaway 2 equal 5?
-        """
+        """ Does 7 takeaway 2 equal 5? """
         self.assertEqual(diff_int(7, 2), 5)
 
     def test_prod(self):
-        """
-        Does 2 times 7 equal 14?
-        """
+        """ Does 2 times 7 equal 14? """
         self.assertEqual(prod_int(2, 7), 14)
 
 
